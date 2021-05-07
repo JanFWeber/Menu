9cd8643bc955ca795efb371bcad17a3f4fc53b60
JanFWeber <wi20085@lehre.dhbw-stuttgart.de>
2021-05-07 18:54:41 +0200

    Init

--- app/src/main/java/com/example/menubar/MainActivity2.java
+++ app/src/main/java/com/example/menubar/MainActivity2.java
@@ -1,8 +1,15 @@
 package com.example.menubar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +17,33 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item){
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_trending:
+                            selectedFragment = new TrendingFragment();
+                            break;
+                        case R.id.nav_news:
+                            selectedFragment = new NewsFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+
 }
